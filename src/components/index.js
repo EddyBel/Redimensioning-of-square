dc62b3d395f108c90d6e1b4cd706d9c8c1ab0aff
EddyBel <dante61918@gmail.com>
2023-01-14 18:10:27 -0600

    Aggregate documentation

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import { Spinner } from "./spinner";
-import { Button } from "./button";
-import { MoveableComponent } from "./moveables";
-
-export { Spinner, Button, MoveableComponent };
--- src/hooks/useContexts.js
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { MoveableContext } from "../context/moveablesContext";
-import { ImagesContext } from "../context/imgesContext";
-
-export const useImages = () => useContext(ImagesContext);
-export const useMoveables = () => useContext(MoveableContext);
